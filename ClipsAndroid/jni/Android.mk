73bc8b907bd8dd2d78277978d63b8d59124c5d5c
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-06-25 16:35:07 +0200

    Adding utility macro to log CLIPS native code on Android's logcat

--- ClipsAndroid/jni/Android.mk
+++ ClipsAndroid/jni/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := clips
+LOCAL_LDLIBS := -llog
 FILE_LIST := $(wildcard $(LOCAL_PATH)/clips/*.c)
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/clips
---
+++ ClipsAndroid/jni/clips/logcat.h
@@ -0,0 +1,3 @@
+#include <android/log.h>
+#define  LOG_TAG    "CLIPS native code"
+#define  LOGCAT(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
