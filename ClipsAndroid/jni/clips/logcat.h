ddcfae7bf0407df9d549a8dce4013112ed6604d3
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-08-26 15:22:58 +0200

    More comments on the logging macro.

--- ClipsAndroid/jni/clips/logcat.h
+++ ClipsAndroid/jni/clips/logcat.h
@@ -1,7 +1,9 @@
+// Inspired by http://manski.net/2012/05/logging-from-c-on-android/
 #include <android/log.h>
 #define  LOG_TAG    "CLIPS native code"
 #define  aprintf(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-// Or...
+
+// Or as suggested by http://stackoverflow.com/a/10275473
 //#define printf(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-// It should be never define before including stdio!
-// That's too difficult as I don't understand the code and its dependencies so well
+// Problem: It should never be defined before including stdio!
+// That's too difficult as I don't fully understand CLIPS code and its dependencies.
